0d18c3360605ad18fd6cbc8b205dc27220e9e7c1
amankhandelwaal <imakhandelwal2005@gmail.com>
2026-04-16 18:26:15 +0530

    chore: initial project setup

---
+++ src/main/java/com/jeera/jeera/JeeraApplication.java
@@ -0,0 +1,13 @@
+package com.jeera.jeera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeeraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeeraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeera/jeera/JeeraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeera.jeera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeeraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
