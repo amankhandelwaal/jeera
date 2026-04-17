0d64c27365c0ba2712cd5858ce238db9787e0108
amankhandelwaal <imakhandelwal2005@gmail.com>
2026-04-17 10:55:50 +0530

    feat(data): implement spring data jpa repositories

---
+++ src/main/java/com/jeera/repository/NotificationRepository.java
@@ -0,0 +1,15 @@
+package com.jeera.repository;
+
+import com.jeera.model.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+  List<Notification> findByRecipientIdOrderByCreatedAtDesc(Long recipientId);
+
+  List<Notification> findByRecipientIdAndIsReadFalseOrderByCreatedAtDesc(Long recipientId);
+
+  long countByRecipientIdAndIsReadFalse(Long recipientId);
+}
